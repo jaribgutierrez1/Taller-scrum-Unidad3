12ee6f3d5b43b2fe7d729b257d31535f8e71dd6b
jaribgutierrez1 <jgutierr64@gmail.com>
2022-10-09 17:04:32 -0500

    dando formato a componente y llamandolo dentro de appjs

---
+++ components/Formulario.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Formulario(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Formulario;
--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Formulario from '../components/Formulario';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        Formulario
       </header>
     </div>
   );
