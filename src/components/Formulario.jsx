e211f005a825300f36f2d2cc5d6199913b43ec45
jaribgutierrez1 <jgutierr64@gmail.com>
2022-10-09 20:28:40 -0500

    fromulario sin validacion

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,17 +3,20 @@ import { db } from '../firebase';
 import { collection, onSnapshot, addDoc, doc, deleteDoc} from 'firebase/firestore';
 
 const Formulario = () => {
-    const [fruta, setFruta] = useState('');
-    const [descripcion, setDescripcion] = useState('');
-    const [listaFrutas, setListaFrutas] = useState([])
+    const [nombre, setNombre] = useState('');
+    const [apellidos, setApellidos] = useState('');
+    const [edad, setEdad] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [email, setEmail] = useState('');
+    const [lista, setLista] = useState([])
 
 
     useEffect(() => {
 
         const obtenerDatos = async () => {    
           try {
-            await onSnapshot(collection(db, "frutas"), (querySnapshot) => {    
-              setListaFrutas(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+            await onSnapshot(collection(db, "form"), (querySnapshot) => {    
+              setLista(querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
             });
           } catch (error) {
             console.log(error);
@@ -26,29 +29,35 @@ const Formulario = () => {
     const eliminar = async id => {
         console.log(id)
         try{
-            await deleteDoc(doc(db,'frutas',id))
+            await deleteDoc(doc(db,'form',id))
         }catch(error){
             console.log(error)
         }
     }
-    const guardarFrutas = async (e) =>{
+    const guardarclientes = async (e) =>{
         e.preventDefault()
+        
         try{
-       
-           
-            const data = await addDoc(collection(db,'frutas'),{
+
+            const data = await addDoc(collection(db,'form'),{
                 
-                nombreFruta: fruta,
-                nombreDescripcion: descripcion
+                nombrenombre: nombre,
+                nombreapellidos: apellidos,
+                nombreedad:edad,
+                nombretelefono:telefono,
+                nombreemail:email
             })
 
-            setListaFrutas([
-                ...listaFrutas,
-                {nombreFruta:fruta, nombreDescripcion:descripcion, id:data.id}
+            setLista([
+                ...lista,
+                {nombrenombre:nombre, nombreapellidos:apellidos, nombreedad:edad, nombretelefono:telefono, nombreemail:email, id:data.id}
             ])
 
-            setFruta('')
-            setDescripcion('')
+            setNombre('')
+            setApellidos('')
+            setEdad('')
+            setTelefono('')
+            setEmail('')
             e.target.reset()
         }catch(error){
             console.log(error)
@@ -57,16 +66,16 @@ const Formulario = () => {
 
   return (
     <div className='container mt-5'>
-        <h1 className='text-center'>CRUD BÁSICO</h1>
+        <h1 className='text-center'>FORMULARIO</h1>
         <hr/>
         <div className='row'>
             <div className="col-8">
-                <h4 className="text-center">Listado de Frutas</h4>
+                <h4 className="text-center">Listado de nuevos Clientes</h4>
                 <ul className="list-group">
                     {
-                        listaFrutas.map(item =>(
+                        lista.map(item =>(
                             <li className='list-group-item' key={item.id}>
-                                <span className='lead'>{item.nombreFruta}-{item.nombreDescripcion}</span>
+                                <span className='lead'>{item.nombrenombre}  {item.nombreapellidos}  {item.nombreedad}    {item.nombretelefono}  {item.nombreemail}</span>
                                 <button className='btn btn-danger btn-sm float-end mx-2' onClick={()=>eliminar(item.id)}>Eliminar</button>
                             </li>
                         ))
@@ -74,30 +83,52 @@ const Formulario = () => {
                 </ul>
             </div>
         
-        <div className='col-4'>
+            <div className='col-4'>
             <h4 className="text-center">
-                Agregar Frutas
+                Formulario Cliente Nuevo
             </h4>
-            <form onSubmit={guardarFrutas}>
+            <form onSubmit={guardarclientes}>
                 <input 
                 className='form-control mb-2'
                 type="text" 
-                placeholder='Ingrese Fruta'
-                onChange={(e)=>setFruta(e.target.value)}
-                value = {fruta}
-                ></input>
+                placeholder='Ingrese Nombres'
+                onChange={(e)=>setNombre(e.target.value)}
+                value = {nombre}></input>
+                
                 <input 
                 className='form-control mb-2'
                 type="text" 
-                placeholder='Ingrese Descripción'
-                onChange={(e) => setDescripcion(e.target.value)}
-                value = {descripcion}></input>
+                placeholder='Ingrese Apellidos'
+                onChange={(e) => setApellidos(e.target.value)}
+                value = {apellidos}></input>
+                
+                <input 
+                className='form-control mb-2'
+                type="number" 
+                placeholder='Ingrese Edad (Años)'
+                onChange={(e) => setEdad(e.target.value)}
+                value = {edad}></input>
+                
+                <input 
+                className='form-control mb-2'
+                type="tel" 
+                placeholder='Ingrese Telefono'
+                onChange={(e) => setTelefono(e.target.value)}
+                value = {telefono}></input>
+                
+                <input 
+                className='form-control mb-2'
+                type="email" 
+                placeholder='Ingrese direccion Email'
+                onChange={(e) => setEmail(e.target.value)}
+                value = {email}></input>
+                
                 <button 
                 className='btn btn-primary btn-block'
                 type='submit'
                 >Agregar</button>
             </form>
-        </div>
+            </div>
         </div>   
     </div>
   )
